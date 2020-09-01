09868fb0b7a47831b31b0f1d9eaacbd186d26ff8
Andrei Cozma <andreig992@gmail.com>
2020-09-01 16:58:26 -0400

    Added example program

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import PyQt5
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 import matplotlib
 
 
-def main():
-    print("Hello World")
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Window Title'
+        self.left = 0
+        self.top = 0
+        self.width = 640
+        self.height = 480
+        self.initUI()
 
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left,self.top,self.width,self.height)
+        self.show()
 
-main()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
