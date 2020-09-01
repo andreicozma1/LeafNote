153a928971084a0951798011b9be3c294aa6ad6c
Andrei Cozma <andreig992@gmail.com>
2020-09-01 17:39:36 -0400

    Added main vertical layout, and centralWidget, which allows the box to be resizable for #1

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPlainTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPlainTextEdit, QMainWindow,QVBoxLayout
 from PyQt5.QtGui import QIcon
 import matplotlib
 
 
-class App(QWidget):
+class App(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.title = 'Window Title'
+        super(QMainWindow, self).__init__()
+        # Initialize properties. TODO - make application properties their own class separately
+        self.title = '0x432d2d'
         self.left = 0
         self.top = 0
         self.width = 640
         self.height = 480
-        self.initUI()
+        # Vertical main layout. TODO - The middle section of VBox should be a horizontal 3 section box
+
+        self.setCentralWidget(self.initUI())
+        self.show()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left,self.top,self.width,self.height)
 
-        self.textbox = QPlainTextEdit(self)
-        self.textbox.resize(self.width,self.height)
-        self.show()
+        textbox = QPlainTextEdit(self)
+        textbox.resize(self.width,self.height)
 
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(textbox)
+
+        widget = QWidget()
+        widget.setLayout(self.layout)
+        return widget
+
+
+
+
+# Starting point of the program
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
