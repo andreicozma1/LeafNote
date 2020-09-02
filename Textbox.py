91087d6b4ad6c12ad34e17383380713fcd0e19c2
Hunter Price <hunterprice4044@gmail.com>
2020-09-02 14:54:54 -0400

    Added Editable Text Box as part of master layout #19

--- Textbox.py
+++ Textbox.py
@@ -1,7 +1,8 @@
 from PyQt5.QtGui import QColor, QPalette
 from PyQt5.QtWidgets import QPlainTextEdit
 
-#
+
+# Class to hold and customize a QPlainTextEdit Widget
 class PlainTextEdit(QPlainTextEdit):
     def __init__(self, plain_text=None):
         super(QPlainTextEdit, self).__init__()
@@ -10,9 +11,10 @@ class PlainTextEdit(QPlainTextEdit):
         self.setPlainText(plain_text)
         self.setAutoFillBackground(True)
 
-    # Set the background color after
+    # Set the background color of the QPlainTextEdit Widget
     def setBackgroundColor(self, color):
         palette = self.palette()
         palette.setColor(QPalette.Active, QPalette.Base, QColor(color))
         self.setPalette(palette)
         self.setBackgroundVisible(False)
+
