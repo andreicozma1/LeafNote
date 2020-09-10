ef5719b703db8a7bf2101a013b664102ae6a7965
Hunter Price <hunterprice4044@gmail.com>
2020-09-09 23:07:34 -0400

    reverting previous acciedental changes

--- Document.py
+++ Document.py
@@ -11,25 +11,3 @@ class Document(TextBox):
         self.setMinimumWidth(minWidth)
         self.setBackgroundColor("white")
         self.setTextColorByString("black")
-
-        if textDefault is None:
-            textDefault = "You can type here."
-        print("TextBox - text: ", textDefault)
-
-        self.setPlainText(textDefault)
-        self.setAutoFillBackground(True)
-
-    # Set the background color of the QPlainTextEdit Widget
-    def setBackgroundColor(self, color):
-        palette = self.palette()
-        # Set color for window focused
-        palette.setColor(QPalette.Active, QPalette.Base, QColor(color))
-        # Set color for window out of focus
-        palette.setColor(QPalette.Inactive, QPalette.Base, QColor(color))
-
-        self.setPalette(palette)
-        # self.setBackgroundVisible(False)
-
-    def setTextColorByString(self, color):
-        self.setTextColor(QColor(color))
-
