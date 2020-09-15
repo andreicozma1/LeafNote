15f611028bed3e4d78e325b545b7a9b20e33d3f5
Dani <dshekou1@vols.utk.edu>
2020-09-15 18:34:14 -0400

    changed font to scale and fixed margins

--- src/Elements/BottomBar.py
+++ src/Elements/BottomBar.py
@@ -8,18 +8,21 @@ class BottomBar(QWidget):
         super(BottomBar, self).__init__(*args, **kwargs)
         self.horz = QHBoxLayout()
         self.setLayout(self.horz)
+        self.horz.setContentsMargins(0, 0, 0, 0)
 
         self.l1 = QLabel("Word Count: 0")
         font = self.l1.font()
-        font.setPointSize(8)
+        font.setPointSize(12)
         self.l1.setFont(font)
         self.horz.addWidget(self.l1)
+        self.l1.adjustSize()
 
         self.l2 = QLabel("Character Count: 0")
         font = self.l2.font()
-        font.setPointSize(8)
+        font.setPointSize(12)
         self.l2.setFont(font)
         self.horz.addWidget(self.l2)
+        self.l2.adjustSize()
         #l1.setText("Word Count: ")
         #l1.setAlignment(Qt.AlignLeft)
 
