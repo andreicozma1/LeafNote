4cae4e064d1e784c7eb7adaa246fb86fb37ae86a
Andrei Cozma <andreig992@gmail.com>
2020-09-29 15:27:16 -0400

    Implemented Prompt dialog for converting file to proprietary format for #69

--- src/main/python/Utils/DialogBuilder.py
+++ src/main/python/Utils/DialogBuilder.py
@@ -1,18 +1,35 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout,QWidget
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QWidget, QPushButton, QLayout, QLabel, QDialogButtonBox
 
 
 class DialogBuilder(QDialog):
-    def __init__(self, title):
-        super(DialogBuilder, self).__init__()
+    def __init__(self, blocks, winTitle: str = None, titleText: str = None, msgText: str = None):
+        super(DialogBuilder, self).__init__(blocks)
 
-        if title is None:
-            title = "Dialog"
+        if winTitle is None:
+            winTitle = "Dialog"
 
-        self.setWindowTitle(title)
+        self.setWindowTitle(winTitle)
 
         self.vertical_layout = QVBoxLayout(self)
+        self.title = QLabel(titleText)
+        font = self.title.font()
+        font.setPointSize(18)
+        font.setBold(True)
+        self.title.setFont(font)
+        self.message = QLabel(msgText)
+        self.vertical_layout.addWidget(self.title)
+        self.vertical_layout.addWidget(self.message)
 
-        self.exec()
+    def setTitleText(self, text: str):
+        self.title.setText(text)
 
-    def addWidget(self, widget:QWidget):
+    def setMsgText(self, text: str):
+        self.message.setText(text)
+
+    def addWidget(self, widget: QWidget):
         self.vertical_layout.addWidget(widget)
+
+    def addButtonBox(self, buttonBox: QDialogButtonBox):
+        self.vertical_layout.addWidget(buttonBox)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
