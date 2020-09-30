b6e47f1e121d895c3bfe22b6df3a7b3e3067aa48
Andrei Cozma <andreig992@gmail.com>
2020-09-29 20:10:20 -0400

    Updated behavior for bolding making the text too thin upon unbolding for #84

--- src/main/python/Elements/TopBar.py
+++ src/main/python/Elements/TopBar.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QComboBox, QPushButton, QFontComboBox, QDialogButtonBox
-
+from PyQt5.QtGui import QFont
 from Utils.DialogBuilder import DialogBuilder
 
 
@@ -21,7 +21,7 @@ class TopBar(QWidget):
         # ComboBox for font sizes
         self.combo_font_style = QFontComboBox(self)
         self.combo_font_style.setToolTip('Change font')
-        self.combo_font_style.currentIndexChanged.connect(self.fontChange)
+        self.combo_font_style.currentIndexChanged.connect(self.onFontStyleChanged)
         self.combo_font_style.setFocusPolicy(Qt.NoFocus)
         self.horizontal_layout.addWidget(self.combo_font_style)
 
@@ -31,7 +31,7 @@ class TopBar(QWidget):
         self.combo_font_size.addItems(self.list_FontSize)
         self.combo_font_size.setCurrentIndex(11)
         self.combo_font_size.setFixedWidth(60)
-        self.combo_font_size.currentIndexChanged.connect(self.selectionChange)
+        self.combo_font_size.currentIndexChanged.connect(self.onFontSizeChanged)
         self.combo_font_size.setFocusPolicy(Qt.NoFocus)
         self.horizontal_layout.addWidget(self.combo_font_size)
 
@@ -42,7 +42,7 @@ class TopBar(QWidget):
         self.button_bold.setFixedWidth(33)
         self.button_bold.setStyleSheet("font:Bold")
         self.button_bold.setCheckable(True)
-        self.button_bold.clicked.connect(self.setBold)
+        self.button_bold.clicked.connect(self.onFontBoldChanged)
         self.button_bold.setFocusPolicy(Qt.NoFocus)
         self.horizontal_layout.addWidget(self.button_bold)
 
@@ -53,7 +53,7 @@ class TopBar(QWidget):
         self.button_ital.setFixedWidth(33)
         self.button_ital.setStyleSheet("font:Italic")
         self.button_ital.setCheckable(True)
-        self.button_ital.clicked.connect(self.setItal)
+        self.button_ital.clicked.connect(self.onFontItalChanged)
         self.button_ital.setFocusPolicy(Qt.NoFocus)
         self.horizontal_layout.addWidget(self.button_ital)
 
@@ -64,7 +64,7 @@ class TopBar(QWidget):
         self.button_strike.setFixedWidth(33)
         self.button_strike.setStyleSheet("text-decoration: line-through")
         self.button_strike.setCheckable(True)
-        self.button_strike.clicked.connect(self.setStrike)
+        self.button_strike.clicked.connect(self.onFontStrikeChanged)
         self.button_strike.setFocusPolicy(Qt.NoFocus)
         self.horizontal_layout.addWidget(self.button_strike)
 
@@ -75,7 +75,7 @@ class TopBar(QWidget):
         self.button_under.setFixedWidth(33)
         self.button_under.setStyleSheet("text-decoration: underline")
         self.button_under.setCheckable(True)
-        self.button_under.clicked.connect(self.setUnder)
+        self.button_under.clicked.connect(self.onFontUnderChanged)
         self.button_under.setFocusPolicy(Qt.NoFocus)
         self.horizontal_layout.addWidget(self.button_under)
 
@@ -85,10 +85,10 @@ class TopBar(QWidget):
         # Mode Switching button to the very right (after stretch)
         self.button_mode_switch = QPushButton("Formatting Mode", self)
         self.button_mode_switch.setToolTip("Enable Document Formatting")
-        self.button_mode_switch.setProperty("persistent", True)  # Used to keep button enabled in queryEnableFormatting
+        self.button_mode_switch.setProperty("persistent", True)  # Used to keep button enabled in onEnableFormatting
         self.button_mode_switch.setCheckable(True)
         self.button_mode_switch.setFocusPolicy(Qt.NoFocus)
-        self.button_mode_switch.clicked.connect(self.queryEnableFormatting)
+        self.button_mode_switch.clicked.connect(self.onEnableFormatting)
         self.horizontal_layout.addWidget(self.button_mode_switch)
 
         self.setup()
@@ -105,8 +105,8 @@ class TopBar(QWidget):
         return self
 
     # Toggles between Formatting Mode and Plain-Text Mode
-    def queryEnableFormatting(self, state):
-        print("TopBar - queryEnableFormatting -", state)
+    def onEnableFormatting(self, state):
+        print("TopBar - onEnableFormatting -", state)
 
         if state is True:
             convert_dialog = DialogBuilder(self.document.layout, "Enable Formatting",
@@ -117,16 +117,16 @@ class TopBar(QWidget):
             buttonBox = QDialogButtonBox(QDialogButtonBox.Cancel | QDialogButtonBox.Yes)
             convert_dialog.addButtonBox(buttonBox)
             if convert_dialog.exec():
-                print("TopBar - queryEnableFormatting - User converted file to Proprietary Format")
+                print("TopBar - onEnableFormatting - User converted file to Proprietary Format")
                 # TODO - Convert file with FileManager to a .lef format, on success, call the function below
                 self.setFormattingEnabled(True)
             else:
-                print("TopBar - queryEnableFormatting - User DID NOT convert file to Proprietary Format")
+                print("TopBar - onEnableFormatting - User DID NOT convert file to Proprietary Format")
                 self.button_mode_switch.setChecked(False)
         else:
             # Don't allow converted file to be converted back to Plain Text
             # TODO - allow option to save different file as plain text, or allow conversion back but discard formatting options
-            print("TopBar - queryEnableFormatting - Cannot convert back to Plain Text")
+            print("TopBar - onEnableFormatting - Cannot convert back to Plain Text")
             self.button_mode_switch.setChecked(True)
 
     def setFormattingEnabled(self, state):
@@ -136,30 +136,29 @@ class TopBar(QWidget):
                 a.setEnabled(state)
 
     # Sets the font to the new font
-    def fontChange(self):
+    def onFontStyleChanged(self):
         self.document.setCurrentFont(self.combo_font_style.currentFont())
 
     # Sets the current sets the font size from the ComboBox
-    def selectionChange(self):
+    def onFontSizeChanged(self):
         self.document.setFontPointSize(int(self.combo_font_size.currentText()))
 
     # Sets the font to italic
-    def setItal(self):
+    def onFontItalChanged(self):
         self.document.setFontItalic(self.button_ital.isChecked())
 
     # Sets the font to bold
-    def setBold(self):
-        if self.button_bold.isChecked():
-            self.document.setFontWeight(75)
-        else:
-            self.document.setFontWeight(25)
+    def onFontBoldChanged(self):
+        self.document.setFontWeight(QFont.Bold if self.button_bold.isChecked() else QFont.Normal)
 
     # Sets the font to underlined
-    def setUnder(self):
+    def onFontUnderChanged(self):
         self.document.setFontUnderline(self.button_under.isChecked())
 
     # Sets the font to strike
-    def setStrike(self):
-        f = self.document.currentCharFormat()
-        f.setFontStrikeOut(self.button_strike.isChecked())
-        self.document.setCurrentCharFormat(f)
+    def onFontStrikeChanged(self):
+        fontFormat = self.document.currentCharFormat()
+        fontFormat.setFontStrikeOut(self.button_strike.isChecked())
+        self.document.setCurrentCharFormat(fontFormat)
+
+
