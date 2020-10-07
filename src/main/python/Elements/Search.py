66dc1bf71c854704b234c4c98dd66f2bdc8907ce
Hunter Price <hunterprice4044@gmail.com>
2020-10-06 20:50:09 -0400

    added working search bar #8

--- src/main/python/Elements/Search.py
+++ src/main/python/Elements/Search.py
@@ -1,8 +1,9 @@
 import logging
+import os
 
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QTextDocument
+from PyQt5.QtCore import Qt, QRegExp
+from PyQt5.QtGui import QTextDocument, QPixmap, QIcon, QTransform
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLineEdit, QPushButton, QTextEdit
 
 
@@ -10,6 +11,8 @@ class Find(QWidget):
     def __init__(self, document):
         super(Find, self).__init__()
         self.document = document
+        self.search = ""
+        self.flags = QTextDocument.FindFlag(0)
 
         self.initUI()
 
@@ -34,38 +37,93 @@ class Find(QWidget):
         self.search_bar.setFixedWidth(200)
         self.horizontal_layout.addWidget(self.search_bar, 0, Qt.AlignLeft)
 
+        def createSearchBtn(title, tool_tip, on_click, isCheckable: bool = True):
+            btn = QPushButton(title)
+            btn.setContentsMargins(0, 0, 0, 0)
+            btn.setToolTip(tool_tip)
+            btn.setCheckable(isCheckable)
+            btn.setFlat(True)
+            btn.setFixedWidth(25)
+            btn.clicked.connect(on_click)
+            return btn
+
         # add the case sensitive option
-        self.case_sensitive = QPushButton("Aa")
-        self.case_sensitive.setContentsMargins(0, 0, 0, 0)
-        self.case_sensitive.setToolTip("Match Case")
-        self.case_sensitive.setCheckable(True)
-        self.case_sensitive.setFlat(True)
-        self.case_sensitive.setFixedWidth(25)
+        self.case_sensitive = createSearchBtn("Aa", "Match Case", self.onCaseSensitiveSearchSelect)
         self.horizontal_layout.addWidget(self.case_sensitive)
 
         # add the case sensitive option
-        self.whole_word = QPushButton("W")
-        self.whole_word.setContentsMargins(0, 0, 0, 0)
-        self.whole_word.setToolTip("Match Case")
-        self.whole_word.setCheckable(True)
-        self.whole_word.setFlat(True)
-        self.whole_word.setFixedWidth(30)
+        self.whole_word = createSearchBtn("W", "Words", self.onWholeWordSearchSelect)
         self.horizontal_layout.addWidget(self.whole_word)
 
+        # add the case sensitive option
+        self.regex_search = createSearchBtn(".*", "Regex", self.onRegexSearchSelect)
+        self.horizontal_layout.addWidget(self.regex_search)
+
+        # get required images
+        path = os.path.join("resources", "arrow.ico")
+        pixmap = QPixmap(path)
+        down_arrow = QIcon(pixmap)
+        up_arrow = QIcon(pixmap.transformed(QTransform().rotate(180)))
+
+        # add the previous occurrence option
+        self.previous_occurrence = createSearchBtn("", "Previous Occurrence", self.onPreviousOccurrenceSelect, False)
+        self.previous_occurrence.setIcon(up_arrow)
+        self.horizontal_layout.addWidget(self.previous_occurrence)
+
+        # add the next occurrence option
+        self.next_occurrence = createSearchBtn("", "Next Occurrence", self.onNextOccurrenceSelect, False)
+        self.next_occurrence.setIcon(down_arrow)
+        self.horizontal_layout.addWidget(self.next_occurrence)
+
+
+    def onCaseSensitiveSearchSelect(self):
+        if self.regex_search.isChecked():
+            self.case_sensitive.setChecked(False)
+        self.onChanged(self.search_bar.text())
+
+    def onWholeWordSearchSelect(self):
+        if self.regex_search.isChecked():
+            self.whole_word.setChecked(False)
+        self.onChanged(self.search_bar.text())
+
+    def onRegexSearchSelect(self):
+        if self.regex_search.isChecked():
+            self.case_sensitive.setChecked(False)
+            self.whole_word.setChecked(False)
+        self.onChanged(self.search_bar.text())
+
+    def onPreviousOccurrenceSelect(self):
+        logging.info(self.document.find(self.search, self.flags | QTextDocument.FindBackward))
+
+    def onNextOccurrenceSelect(self):
+        logging.info(self.document.find(self.search, self.flags))
+
     # when text is entered in search through the document
-    def onChanged(self, text):
-        logging.info(text)
-        file_data = self.document.toPlainText()
-        flags = QTextDocument.FindFlag(0)
-        # if text is not case sensitive
+    def onChanged(self, search):
+        logging.info(search)
+        self.search = search
+
+        # set the cursor to the beginning of the document
+        cursor = self.document.textCursor()
+        cursor.setPosition(0)
+        self.document.setTextCursor(cursor)
+
+        # set up the default search flags
+        self.flags = QTextDocument.FindFlag(0)
+
+        # if search is case sensitive
         if self.case_sensitive.isChecked():
-            flags = flags | QTextDocument.FindCaseSensitively
+            self.flags = self.flags | QTextDocument.FindCaseSensitively
 
+        # if search is whole word sensitive
         if self.whole_word.isChecked():
-            flags = flags | QTextDocument.FindWholeWords
+            self.flags = self.flags | QTextDocument.FindWholeWords
 
-        print(self.document.find(text, flags))
-        print(file_data)
+        # if the user IS searching for regex
+        if self.regex_search.isChecked():
+            self.search = QRegExp(self.search)
+
+        logging.info(self.document.find(self.search, self.flags))
 
 
 class FindAll(QWidget):
