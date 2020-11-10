c93f2f37bba2b589aee7a1617648f01f67f57f57
Jacob Isber <jisber@vols.utk.edu>
2020-11-10 01:44:30 -0500

    Unit test for Reminders.py #329

---
+++ src/test/test_reminders.py
@@ -0,0 +1,48 @@
+"""
+test Reminders behaviors.
+"""
+import unittest
+
+from PyQt5.QtWidgets import QApplication, QComboBox, QFontComboBox, QAbstractButton
+from LeafNote import Utils
+from LeafNote import App
+from PyQt5.QtCore import QDate
+
+ctx = QApplication([])
+app = App(ctx)
+
+
+class TestReminders(unittest.TestCase):
+    """
+    Unit test for all Bottom Bar functionality
+    """
+
+    def setUp(self):
+        """
+        Set up environment
+        """
+        self.bottom_bar = app.bottom_bar
+        self.reminders = app.reminders
+        self.document = app.document
+        self.doc_props = app.doc_props
+        self.layout_props = app.layout_props
+
+    def test24convert(self):
+        self.assertEqual(self.reminders.convert24("1:00 PM"), "13:00 ")
+
+    def testAddReminder(self):
+        date: QDate = QDate.currentDate()
+        title = "Test Reminders"
+        description = "Test description"
+        time_r = "13:00"
+
+        self.assertEqual(self.reminders.addReminder(date, time_r, title, description), True)
+
+        dictionary = self.reminders.rem_list
+        reminders_list = list(dictionary.values())
+        reminders_list.sort(key=lambda t: t['sort'])
+
+        for rem in reminders_list:
+            self.key = rem['key']
+
+        self.reminders.deleteReminder(self.key)
