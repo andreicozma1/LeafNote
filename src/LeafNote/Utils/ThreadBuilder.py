ad29d7df05742b564569ef85e96aba1dd119fde3
Hunter Price <hunterprice4044@gmail.com>
2020-11-11 22:35:45 -0500

    reimplpemented the threading for summarizer #351

--- src/LeafNote/Utils/ThreadBuilder.py
+++ src/LeafNote/Utils/ThreadBuilder.py
@@ -8,11 +8,13 @@ class ExecuteThread(QThread):
         super().__init__()
         self.target = target
         self.args = args
+        self.callback = callback
         self.return_value = None
-        if callback:
-            self.finished.connect(partial(callback, self.return_value))
 
     def run(self):
         self.return_value = self.target(*self.args)
+        if self.callback:
+            self.finished.connect(partial(self.callback, self.return_value))
+
 
 
